a7ece2092192197e76cebc6915ebd75ebc36f45c
amrxt1 <amrxt11@outlook.com>
2023-05-29 18:20:41 +0530

    Create RESTful routes and Kittens#new

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,23 @@
+class KittensController < ApplicationController
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
