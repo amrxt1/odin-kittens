c797491755e7858c98502c4c03ce16c97955e928
amrxt1 <amrxt11@outlook.com>
2023-05-29 18:43:18 +0530

    Create  Kittens#show and Kittens#create
    Kittens#create redirects to the newly created kitten (if it saves) otherwise redirects to Kittens#new.

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,12 +4,25 @@ class KittensController < ApplicationController
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+        redirect_to @kitten
+    else
+        render :new, status: :unprocessable_entity
+    end
   end
 
   def index
   end
 
   def show
+    @kitten = Kitten.find_by(id: params[:id])
+  
+    if @kitten.nil?
+      redirect_to new_kitten_path, notice: "Kitten does not exist! Create a New one :"
+    else
+      render :show
+    end
   end
 
   def edit
@@ -19,5 +32,10 @@ class KittensController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
